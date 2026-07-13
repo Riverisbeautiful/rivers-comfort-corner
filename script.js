dd8f77ae11bb0260517641526e47498515c8b0f8
Riverisbeautiful <timydogspot102@gmail.com>
2026-07-12 22:12:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-(function(){
-    // Keys configured for your comfort site
-    emailjs.init("RETQEL0saMrVGt7oV");
-})();
+(function(){ emailjs.init("RETQEL0saMrVGt7oV"); })();
 
+// Email logic
 document.getElementById('sendBtn').addEventListener('click', () => {
     const note = document.getElementById('riverNote').value;
-    
-    if(!note) {
-        alert("Please write something first!");
-        return;
-    }
-    
-    emailjs.send("service_xac90mk", "template_q4hqvuc", {
-        message: note
-    }).then(() => {
+    if(!note) { alert("Please write something first!"); return; }
+    emailjs.send("service_xac90mk", "template_q4hqvuc", { message: note }).then(() => {
         alert("Sent to you!");
-        document.getElementById('riverNote').value = ""; // Clear input after sending
-    }).catch((err) => {
-        alert("Oops, something went wrong.");
-        console.error(err);
+        document.getElementById('riverNote').value = "";
     });
+});
+
+// Digital Hug logic
+document.getElementById('hugBtn').addEventListener('click', () => {
+    alert("Sending you a big hug! I'm always here for you.");
+});
+
+// Simple Game: Affirmation Picker
+document.getElementById('gameBtn').addEventListener('click', () => {
+    const messages = [
+        "You make the world brighter just by being in it.",
+        "I'm so lucky to have you in my life.",
+        "Your feelings matter and you are doing great.",
+        "Take a deep breath, you've got this!",
+        "Everything will be okay, and I'm right here with you."
+    ];
+    const randomMsg = messages[Math.floor(Math.random() * messages.length)];
+    document.getElementById('gameOutput').innerText = randomMsg;
 });
